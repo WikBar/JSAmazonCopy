b53ffcde1779700c77264c61648c1a7fefde2c1c
wbart <wbartoszyn@gmail.com>
2023-07-30 21:33:04 +0200

    changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -43,7 +43,7 @@ products.forEach((product)=>{
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -60,18 +60,32 @@ document.querySelector(".js-products-grid").innerHTML=htmlProducts
 
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  let TimeoutId;
   button.addEventListener('click',()=>{
 
 
 
-
-    const productId=button.dataset.productId;
+    const {productId}=button.dataset;
     let matchingItem;
 
     const selector = document.querySelector(`.js-product-quantity-${productId}`);
     let quantity = Number(selector.value);
 
+    const added=document.querySelector(`.js-added-to-cart-${productId}`);
+    added.classList.add('Added');
+    
 
+    clearTimeout(TimeoutId);
+    
+
+    if(added.classList.contains('Added')){
+
+      TimeoutId=setTimeout(()=>{
+        added.classList.remove('Added')
+      },2000)
+      
+
+    }
     cart.forEach((item)=>{
       if(item.productId===productId){
         matchingItem=item;
@@ -83,8 +97,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
        matchingItem.quantity+=quantity; 
       }else{
           cart.push({
-            productId : productId,
-            quantity : quantity
+            productId,
+            quantity
         });
       };
       let totalQuantity=0;
